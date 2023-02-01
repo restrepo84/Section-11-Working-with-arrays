8c8fcc2d8b4c8254d43aac080d1f15a92b4fbba4
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-01 13:30:03 -0700

    The findindex Method

--- script.js
+++ script.js
@@ -183,6 +183,31 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+//* The findindex Method *//
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
+
 //--------------------------------------------//
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
