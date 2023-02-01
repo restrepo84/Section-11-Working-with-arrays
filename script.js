a9ddef0e396d48bab33cec8c93bcce8ad0d359ee
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-01 14:40:56 -0700

    some and every

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ btnLogin.addEventListener("click", function (e) {
   }
 });
 
-//* Implementing Transfers *//
+// //* Implementing Transfers *//
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
@@ -183,7 +183,24 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
-//* The findindex Method *//
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    // Add movement
+    currentAccount.movements.push(amount);
+    // update the UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
+// //* The findindex Method *//
 
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
@@ -501,3 +518,25 @@ GOOD LUCK 😀
 
 // const account = accounts.find((acc) => acc.owner === "Jessica Davis");
 // console.log(account);
+
+//* some and every
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some((mov) => mov === -130));
+
+const anyDeposits = movements.some((mov) => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every((mov) => mov > 0));
+console.log(account4.movements.every((mov) => mov > 0));
+
+// Separate callback
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
